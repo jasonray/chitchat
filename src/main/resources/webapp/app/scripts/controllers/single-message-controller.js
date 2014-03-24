feb38e3185818f13490a91398c92948541d1d25f
Jason Ray <jason.ray@agilex.com>
2014-03-24 09:23:13 -0400

    remove webapp directory (so that I can regenerate it)

--- src/main/resources/webapp/app/scripts/controllers/single-message-controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('webappApp')
-  .controller('SingleMessageController', function ($scope) {
-  		console.log("building controller SingleMessageController");
-		$scope.sampleMessage = {from:'John Smith', to:'Abby Mallard', message:'this is a sample message'};
-  });
